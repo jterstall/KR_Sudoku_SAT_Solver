cddc995f3ed6e368be32dce75c79cac1de147720
jterstall <jeroen_t@hotmail.com>
2017-10-01 16:10:48 +0200

    Additional restrictions efficient solver

--- solver_efficient.py
+++ solver_efficient.py
@@ -30,11 +30,15 @@ def ind_cell_encoding_efficient(encodings, filled_in, N):
             if not cell_filled_in:
                 # Do regular encoding if not filled in, else no encoding needed for individual cells
                 for d in range(1, 10):
-                    d_transform = sudoku_encoding.transform(i, j, d, N)
-                    all_possible_values.append(d_transform)
-                    for other_d in range(d+1, 10):
-                        encodings.append([-d_transform, -sudoku_encoding.transform(i, j, other_d, N)])
-                encodings.append(all_possible_values)
+                    row_contains_digit = check_row_for_digit(i, d, filled_in, N)
+                    col_contains_digit = check_col_for_digit(j, d, filled_in, N)
+                    block_contains_digit = check_block_for_digit(i, j, d, filled_in, N)
+                    if not any((row_contains_digit, col_contains_digit, block_contains_digit)):
+                        d_transform = sudoku_encoding.transform(i, j, d, N)
+                        for other_d in range(d+1, 10):
+                            other_d_transform = sudoku_encoding.transform(i, j, other_d, N)
+                            if other_d_transform not in filled_in:
+                                encodings.append([-d_transform, -other_d_transform])
     return encodings
 
 def check_row_for_digit(row, d, filled_in, N):
@@ -51,12 +55,13 @@ def row_cell_encoding_efficient(encodings, filled_in, N):
             if not row_contains_digit:
                 for j in range(N):
                     col_contains_digit = check_col_for_digit(j, d, filled_in, N)
-                    if not col_contains_digit:
-                        d_transform = sudoku_encoding.transform(i, j, d, N)                
-                        all_possible_values.append(d_transform)
+                    block_contains_digit = check_block_for_digit(i, j, d, filled_in, N)
+                    if not any((col_contains_digit, block_contains_digit)):
+                        d_transform = sudoku_encoding.transform(i, j, d, N)   
+                        all_possible_values.append(d_transform) 
                         for other_j in range(j+1, N):
                             other_j_transform = sudoku_encoding.transform(i, other_j, d, N)
-                            if not other_j_transform in filled_in:
+                            if other_j_transform not in filled_in:
                                 encodings.append([-d_transform, -other_j_transform])
                 encodings.append(all_possible_values)
     return encodings
@@ -75,20 +80,23 @@ def col_cell_encoding_efficient(encodings, filled_in, N):
             if not col_contains_digit:
                 for i in range(N):
                     row_contains_digit = check_row_for_digit(i, d, filled_in, N)
-                    if not row_contains_digit:
+                    block_contains_digit = check_block_for_digit(i, j, d, filled_in, N)
+                    if not any((row_contains_digit, block_contains_digit)):
                         d_transform = sudoku_encoding.transform(i, j, d, N)               
                         all_possible_values.append(d_transform)
                         for other_i in range(i+1, N):
                             other_i_transform = sudoku_encoding.transform(other_i, j, d, N)
-                            if not other_i_transform in filled_in:
+                            if other_i_transform not in filled_in:
                                 encodings.append([-d_transform, -other_i_transform])
                 encodings.append(all_possible_values)
     return encodings
 
 def check_block_for_digit(i, j, d, filled_in, N):
+    starting_i = i - (i % 3)
+    starting_j = j - (j % 3)
     for k in range(0, 3):
         for l in range(0, 3):
-            if(sudoku_encoding.transform(i+k, j+l, d, N) in filled_in):
+            if(sudoku_encoding.transform(starting_i+k, starting_j+l, d, N) in filled_in):
                 return True
     return False
     
@@ -101,10 +109,15 @@ def block_cell_encoding_efficient(encodings, filled_in, N):
                 if not block_contains_digit:
                     for k in range(0, 3):
                         for l in range(0, 3):
-                            d_transform = sudoku_encoding.transform(i+k, j+l, d, N)
-                            all_possible_values.append(d_transform)
-                            for other_d in range(d+1, 10):
-                                encodings.append([-d_transform, -sudoku_encoding.transform(i+k, j+l, other_d, N)])  
+                            row_contains_digit = check_row_for_digit(i+k, d, filled_in, N)
+                            col_contains_digit = check_col_for_digit(j+l, d, filled_in, N)
+                            if not any((row_contains_digit, col_contains_digit)):
+                                d_transform = sudoku_encoding.transform(i+k, j+l, d, N)
+                                all_possible_values.append(d_transform)
+                                for other_d in range(d+1, 10):
+                                    other_d_transform = sudoku_encoding.transform(i+k, j+l, other_d, N)
+                                    if other_d_transform not in filled_in:
+                                        encodings.append([-d_transform, -sudoku_encoding.transform(i+k, j+l, other_d, N)])  
                     encodings.append(all_possible_values)
     return encodings
 
